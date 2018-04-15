7deb95d8e2f44e0ffab045f5f9310586bc174b73
sg-chrishasz <chris@sitego.co>
2018-04-14 22:30:45 -0700

    Cleaned up files

--- src/factory/downloadFileOptionsFactory.ts
+++ src/factory/downloadFileOptionsFactory.ts
@@ -19,16 +19,12 @@ export class DownloadFileOptionsFactory {
 
     public createFileOptions() : ISPPullOptions{
         let sharePointSiteUrl : Uri = Uri.parse(vscode.window.spgo.config.sharePointSiteUrl);
-        let remoteFolder : string = this.glob.path.dirname;//.orig;
+        let remoteFolder : string = this.glob.path.dirname;
         let camlQuery : string = GlobToCamlConverter.Convert(this.glob);
 
         let options : ISPPullOptions = {
-            spBaseFolder : sharePointSiteUrl.path === '' ? '/' : sharePointSiteUrl.path, 
-            //spRootFolder : remoteFolder.replace('/**/','/'),
+            spBaseFolder : sharePointSiteUrl.path === '' ? '/' : sharePointSiteUrl.path,
             dlRootFolder: window.spgo.config.workspaceRoot,
-            //recursive : this.glob.is.globstar,
-            //createEmptyFolders : this.glob.is.globstar,
-            //camlCondition : camlQuery == '' ? null : camlQuery
         }
 
         if(this.glob.is.glob){
@@ -48,58 +44,6 @@ export class DownloadFileOptionsFactory {
             options.recursive = false;
             options.createEmptyFolders = false;
         }
-
-        // let options : ISPPullOptions = {
-        //     spBaseFolder : sharePointSiteUrl.path === '' ? '/' : sharePointSiteUrl.path, 
-        //     //spRootFolder : remoteFolder.replace('/**/','/'),
-        //     dlRootFolder: window.spgo.config.workspaceRoot,
-        //     //recursive : this.glob.is.globstar,
-        //     createEmptyFolders : this.glob.is.globstar,
-        //     camlCondition : camlQuery == '' ? null : camlQuery
-        //     /*
-        //     spHostName?: string;
-        //     spRelativeBase?: string;
-        //     spRootFolder?: string;
-        //     spBaseFolder?: string;
-        //     dlRootFolder?: string;
-        //     recursive?: boolean;
-        //     foderStructureOnly?: boolean;
-        //     createEmptyFolders?: boolean;
-        //     metaFields?: string[];
-        //     restCondition?: string;
-        //     muteConsole?: boolean;
-        //     camlCondition?: string;
-        //     spDocLibUrl?: string;
-        //     omitFolderPath?: string;
-        //     strictObjects?: string[];
-        //     */
-        // };
-
-        // let options : ISPPullOptions = {
-        //     spBaseFolder : sharePointSiteUrl.path === '' ? '/' : sharePointSiteUrl.path, 
-        //     spRootFolder : remoteFolder.replace('/**/','/'),
-        //     dlRootFolder: window.spgo.config.workspaceRoot,
-        //     recursive : this.glob.is.globstar,
-        //     createEmptyFolders : this.glob.is.globstar,
-        //     camlCondition : camlQuery == '' ? null : camlQuery
-        //     /*
-        //     spHostName?: string;
-        //     spRelativeBase?: string;
-        //     spRootFolder?: string;
-        //     spBaseFolder?: string;
-        //     dlRootFolder?: string;
-        //     recursive?: boolean;
-        //     foderStructureOnly?: boolean;
-        //     createEmptyFolders?: boolean;
-        //     metaFields?: string[];
-        //     restCondition?: string;
-        //     muteConsole?: boolean;
-        //     camlCondition?: string;
-        //     spDocLibUrl?: string;
-        //     omitFolderPath?: string;
-        //     strictObjects?: string[];
-        //     */
-        // };
 
         return options;
     }
