1db4209d0dbcf70ea1b3eb52b08ab450404df7be
Charles Pritchard <charles.pritchard@adm-computing.co.uk>
2019-07-01 14:54:55 +0100

    Prettified

--- src/util/fileHelper.ts
+++ src/util/fileHelper.ts
@@ -2,12 +2,12 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-export class FileHelper{
-    static getFileName(filePath : string) : string{
+export class FileHelper {
+    static getFileName(filePath: string): string {
         return filePath.substring(filePath.lastIndexOf(path.sep) + 1);
     }
 
-    static getFolderFromPath(filePath : string) : string{
+    static getFolderFromPath(filePath: string): string {
         let relativeFilePath = filePath.split(vscode.window.spgo.config.workspaceRoot + path.sep)[1].toString();
         return relativeFilePath.substring(0, relativeFilePath.lastIndexOf(path.sep));
     }
