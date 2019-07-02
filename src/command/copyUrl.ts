1c4a8695e49bc418eaaebcc22ae68a9cc3fcbe08
Charles Pritchard <charles.pritchard@adm-computing.co.uk>
2019-07-02 10:40:04 +0100

    Copy url functionality

--- src/command/copyUrl.ts
+++ src/command/copyUrl.ts
@@ -1,11 +1,19 @@
+'use strict';
 import * as vscode from 'vscode';
+import { FileHelper } from '../util/fileHelper';
 
 /**
 * Copies the SharePoint URL of either the file from context menu or the currently active editor
-* @param {vscode.Uri} resourcePath - Optional filepath (Uses current editor if not given
+* @param {vscode.Uri} resourcePath - Optional filepath (Uses current editor if not given)
 * @param {boolean} absolute - Option to copy the absolute path (defaults to SPGo Config)
 */
-export default function copySPUrl(resourcePath?: vscode.Uri, absolute: boolean = false) {
-  console.log("Resource: ", resourcePath.fsPath);
-  console.log("Absolute: ", absolute);
+export default async function copySPUrl(activeResource?: vscode.Uri, absolute: boolean = false): Promise<void> {
+  let editorFile = FileHelper.getActiveFile(vscode.workspace.workspaceFolders);
+  let filePath = activeResource && activeResource.fsPath || editorFile && editorFile.fileName;
+  if (!filePath) return vscode.window.showErrorMessage("SPGo: No file/folder selected") && undefined;
+  const config = vscode.window.spgo.config;
+  if (filePath.indexOf(config.workspaceRoot) == -1) return vscode.window.showErrorMessage("SPGo: File not in source directory") && undefined
+  let serverRelativePath = filePath.replace(config.workspaceRoot, "").replace(/\\/g, "/");
+  vscode.env.clipboard.writeText(absolute ? config.sharePointSiteUrl + serverRelativePath : serverRelativePath);
+  vscode.window.showInformationMessage("SPGo: URL copied to clipboard");
 }
