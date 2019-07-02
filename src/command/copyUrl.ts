abf188379ad273c916ba1b30341a33c394474516
Charles Pritchard <charles.pritchard@adm-computing.co.uk>
2019-07-02 11:00:31 +0100

    Comments

--- src/command/copyUrl.ts
+++ src/command/copyUrl.ts
@@ -8,12 +8,15 @@ import { FileHelper } from '../util/fileHelper';
 * @param {boolean} absolute - Option to copy the absolute path (defaults to SPGo Config)
 */
 export default async function copySPUrl(activeResource?: vscode.Uri, absolute: boolean = false): Promise<void> {
+  //define the active file or folder
   let editorFile = FileHelper.getActiveFile(vscode.workspace.workspaceFolders);
   let filePath = activeResource && activeResource.fsPath || editorFile && editorFile.fileName;
   if (!filePath) return vscode.window.showErrorMessage("SPGo: No file/folder selected") && undefined;
+  //check file is in workspace
   const config = vscode.window.spgo.config;
   if (filePath.indexOf(config.workspaceRoot) == -1) return vscode.window.showErrorMessage("SPGo: File not in source directory") && undefined
   let serverRelativePath = filePath.replace(config.workspaceRoot, "").replace(/\\/g, "/");
+  //copy to clipboard and emit message
   vscode.env.clipboard.writeText(absolute ? config.sharePointSiteUrl + serverRelativePath : serverRelativePath);
   vscode.window.showInformationMessage("SPGo: URL copied to clipboard");
 }
